ae1612217292beb2b236eaff0d185ea77da0c465
Afei <afeibaili@outlook.com>
2026-03-03 01:12:35 +0800

    更新打印日志

--- core/src/main/kotlin/online/afeibaili/mchat/socket/Reader.kt
+++ core/src/main/kotlin/online/afeibaili/mchat/socket/Reader.kt
@@ -31,7 +31,7 @@ class Reader(
                 while (reader.readLine().also { line = it } != null) {
                     action(cipher.decrypt(line))
                 }
-                throw RuntimeException("mchat server disconnect")
+                throw RuntimeException("Mchat server disconnect")
             }.onFailure { exception ->
                 exception.printStackTrace()
                 catch(exception)
