8b16c3d3d55aee79ece854a8b6304c9cd6f2ce2d
Afei <afeibaili@outlook.com>
2026-06-09 07:59:10 +0800

    init commit

--- src/main/java/cn/afeibaili/mchat/MChat.java
+++ src/main/java/cn/afeibaili/mchat/MChat.java
@@ -30,7 +30,7 @@ import static cn.afeibaili.mchat.MChat.mchatSystem;
 public class MChat {
     public static final String MODID = "mchat";
     public static final String NAME = "MChat";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0-forge-1.12.2";
 
     public static MChatSystem mchatSystem = new MChatSystem();
 
